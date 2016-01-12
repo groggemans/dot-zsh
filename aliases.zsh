021a2092359672493da8cffdd52021de5eb65546
G. Roggemans <g.roggemans@grog.be>
2016-01-12 20:37:40 +0100

    Added osx specific aliases

--- aliases.zsh
+++ aliases.zsh
@@ -40,3 +40,13 @@ alias lll='ll|less'
 alias lal='la|less'
 
 ##############################################################################
+# OSX specific aliases
+
+if [[ $IS_MAC -eq 1 ]]; then
+    alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES;
+                    killall Finder /System/Library/CoreServices/Finder.app'
+    alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO;
+                    killall Finder /System/Library/CoreServices/Finder.app'
+fi
+
+##############################################################################
