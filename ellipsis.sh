3506f7845412effed710752ba963f08e07b5df00
G. Roggemans <g.roggemans@grog.be>
2017-02-25 20:11:08 +0100

    Use improved update strategy

--- ellipsis.sh
+++ ellipsis.sh
@@ -7,7 +7,7 @@
 ##############################################################################
 
 # Minimal ellipsis version
-ELLIPSIS_VERSION_DEP='1.9.0'
+ELLIPSIS_VERSION_DEP='1.9.4'
 
 # Package dependencies (informational/not used!)
 ELLIPSIS_PKG_DEPS='groggemans/dot-shell'
@@ -33,6 +33,18 @@ pkg.link() {
     # Link package into ~/.config/zsh
     mkdir -p "$ELLIPSIS_HOME/.config"
     fs.link_file "$PKG_PATH" "$ELLIPSIS_HOME/.config/zsh"
+}
+
+##############################################################################
+
+pkg.pull() {
+    # Use improved update strategy
+    git remote update 2>&1 > /dev/null
+    if git.is_behind; then
+        pkg.unlink
+        git.pull
+        pkg.link
+    fi
 }
 
 ##############################################################################
